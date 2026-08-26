ef86bb793bbe36f5d42d6258045b9c45dff11857
Giovanni Bocchi <giovanni.bocchi@kiwidatascience.com>
2026-08-26 17:35:21 +0200

    updates

--- static/gallery/tog-messages-mobile-application/images.js
+++ static/gallery/tog-messages-mobile-application/images.js
@@ -1,2 +1,2 @@
 window.__galleryImages = window.__galleryImages || {};
-window.__galleryImages["tog-messages-mobile-application"] = [];
+window.__galleryImages["tog-messages-mobile-application"] = ["0.png", "1.png", "2.png", "3.png"];
