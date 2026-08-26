a596f980d1f143e2c773c44afe7478403f6af038
Giovanni Bocchi <giovanni.bocchi@kiwidatascience.com>
2026-08-27 00:41:59 +0200

    updates

--- static/gallery/risvegino-mobile-application/images.js
+++ static/gallery/risvegino-mobile-application/images.js
@@ -1,2 +1,2 @@
 window.__galleryImages = window.__galleryImages || {};
-window.__galleryImages["risvegino-mobile-application"] = [];
+window.__galleryImages["risvegino-mobile-application"] = ["0.png", "1.png", "2.png", "3.png"];
