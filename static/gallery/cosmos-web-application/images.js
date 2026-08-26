bbd08a738b2a35c87dceb4ef773514b4e918d80f
Giovanni Bocchi <giovanni.bocchi@kiwidatascience.com>
2026-08-26 16:13:22 +0200

    cosmos updates

--- static/gallery/cosmos-web-application/images.js
+++ static/gallery/cosmos-web-application/images.js
@@ -1,2 +1,2 @@
 window.__galleryImages = window.__galleryImages || {};
-window.__galleryImages["cosmos-web-application"] = [];
+window.__galleryImages["cosmos-web-application"] = ["agent-chat.png", "agents-grid.png", "calendar-view.png", "dashboard.png", "model-provider-panel.png", "routines.png"];
