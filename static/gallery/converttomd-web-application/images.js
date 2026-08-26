23bebe416cbb1c77036d460c3d7e4bf3689d46ad
Giovanni Bocchi <giovanni.bocchi@kiwidatascience.com>
2026-08-26 15:58:20 +0200

    convert2md project

--- static/gallery/converttomd-web-application/images.js
+++ static/gallery/converttomd-web-application/images.js
@@ -1,2 +1,2 @@
 window.__galleryImages = window.__galleryImages || {};
-window.__galleryImages["converttomd-web-application"] = ["0.png", "1.png"];
+window.__galleryImages["converttomd-web-application"] = ["0.png", "1.png", "2.png"];
