af343733e428bdce07ca083186264e6d73bf1d3e
Giovanni Bocchi <giovanni.bocchi@kiwidatascience.com>
2026-08-27 00:27:38 +0200

    updates

--- static/gallery/dr-strange/images.js
+++ static/gallery/dr-strange/images.js
@@ -1,2 +1,2 @@
 window.__galleryImages = window.__galleryImages || {};
-window.__galleryImages["dr-strange"] = ["1.png", "2.png"];
+window.__galleryImages["dr-strange"] = ["0.png", "1.png", "2.png"];
